34ea9a45420796103b7be510975d72a81ccf937f
Valetin <Valentin.romanov161@gmail.com>
2021-01-14 19:01:59 +0300

    Download_and_format.sh

---
+++ Linux/Bash/scripts/download_and_format.sh
@@ -0,0 +1,42 @@
+# На основе case.
+# Первый параметр download . Скачать страницу в файл с сайта https://sjmulder.nl/en/textonly.html
+# Второй параметр parse . Из файла удалить всё содержимое между тегами <style> , затем удалить все html теги из файла
+# Использовать утилиты: curl, sed
+
+
+
+#!/bin/bash
+
+clear
+
+
+function download()
+{
+curl -o index.html https://sjmulder.nl/en/textonly.html
+}
+
+function parse()
+{
+sed -e '/<style>/,/<\/style>/d' -e :a -e 's/<[^>]*>//g;/</N;//ba' index.html
+}
+
+
+
+######
+#MAIN#
+######
+
+function=$1
+
+if [[ $function == "download" ]] || [[ $function == "parse" ]]; then
+  case $function in
+    download)
+      download;;
+    parse)
+      parse;;
+    *)
+      echo "no correct function!"
+  esac
+else
+  echo "no correct function!"
+fi
